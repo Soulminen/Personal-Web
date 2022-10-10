37115446f977503c4e54b2f97d41a85b10420aa1
Yamil Escobar <joseyamilej@hotmail.com>
2022-10-10 13:28:49 +0200

    Fixing navbar and picture

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+window.onscroll = () => {
+    myFunction();
+}
 
+const mainNavigation = document.querySelector('.main-navigation');
 
+const sticky = mainNavigation.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        mainNavigation.classList.add("sticky")
+    } else {
+        mainNavigation.classList.remove("sticky");
+    }
+  }
